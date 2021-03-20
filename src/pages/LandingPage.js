7974ca426248a66bd217f46960abc299c51b1113
Andrés Morales <mora200217@hotmail.com>
2021-03-20 13:20:23 -0500

    Main Config

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,16 @@
+/*
+	Landing Page v0.1 (20-03-2020)
+	Página principal de la aplicación. Contiene toda 
+	la información del a rama. 
+*/ 
+
+import { React } from 'react'; 
+import '@styles/LandingPage.sass'; 
+
+export const LandingPage = () => {
+	return (
+		<h1> Hola mundo soy un Landing Page</h1>
+	); 
+}
+
+
